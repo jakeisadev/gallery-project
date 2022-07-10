edde32fe99366a7169ab16f56673f69f2f315c79
jakeisadev <starowicz@protonmail.com>
2022-07-10 15:18:46 -0400

    finished gallery app code first attempt

---
+++ photo_gallery_v5/js/app.js
@@ -0,0 +1,15 @@
+let searchBox = document.querySelector('#search');
+let pictures = document.querySelectorAll('a');
+
+searchBox.addEventListener("keyup", (event) => {
+  for(let i = 0; i < pictures.length; i++) {
+    if(searchBox.value.lenth == 0) {
+        pictures[i].style.display = "inline";
+    }
+    else if(pictures[i].dataset.title.includes(searchBox.value)) {
+      pictures[i].style.display = "inline";
+    } else {
+      pictures[i].style.display = "none";
+    }
+  }
+});
