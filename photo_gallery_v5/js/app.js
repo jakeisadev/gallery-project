604c872dc862010b4449ea9c9562a0c5024fd640
jakeisadev <starowicz@protonmail.com>
2022-07-12 19:21:47 -0400

    Fixed HTML, CSS, and JS to meet Exceeds expectations criteria.

--- photo_gallery_v5/js/app.js
+++ photo_gallery_v5/js/app.js
@@ -7,10 +7,11 @@ let pictures = document.querySelectorAll('a');
 
 searchBox.addEventListener("keyup", (event) => {
   for(let i = 0; i < pictures.length; i++) {
+    const caption = pictures[i].dataset.caption.toLowerCase();
     if(searchBox.value.lenth == 0) {
         pictures[i].style.display = "inline";
     }
-    else if(pictures[i].dataset.caption.includes(searchBox.value.toLowerCase())) {
+    else if(caption.includes(searchBox.value.toLowerCase())) {
       pictures[i].style.display = "inline";
     } else {
       pictures[i].style.display = "none";
